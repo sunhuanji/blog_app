c76d1f206b30445b6e0f3485c8d8650d889009b5
Sun Huanji <sunhuanji1992@gmail.com>
2017-05-27 21:26:52 +0900

    change user and post models

--- app/models/lianxi.rb
+++
@@ -1,2 +0,0 @@
-class Lianxi < ApplicationRecord
-end
--- db/migrate/20170527064213_create_lianxis.rb
+++
@@ -1,9 +0,0 @@
-class CreateLianxis < ActiveRecord::Migration[5.1]
-  def change
-    create_table :lianxis do |t|
-      t.string
-
-      t.timestamps
-    end
-  end
-end
