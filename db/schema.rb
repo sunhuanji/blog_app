0e77f88e2da76e0bafbfec3fef582cc03cba56b5
Sun Huanji <sunhuanji1992@gmail.com>
2017-06-04 18:03:13 +0900

    erb to slim

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170604074223) do
 
   create_table "ckeditor_assets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -26,8 +25,9 @@ ActiveRecord::Schema.define(version: 20170604074223) do
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "title", null: false
     t.text "body", null: false
+    t.string "title", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
