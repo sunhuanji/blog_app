ac79a26a371be84dc9da6af84613a91b99835ace
taku <otyazuke.tt@gmail.com>
2017-06-04 18:22:36 +0900

    change slim-rails version

--- Gemfile
+++ Gemfile
@@ -76,4 +76,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
-gem 'slim-rails'
+gem 'slim-rails', '3.1.1'
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20170604074223) do
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text "body", null: false
     t.string "title", null: false
+    t.text "body", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
